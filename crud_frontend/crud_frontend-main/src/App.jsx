66c75dd9ecd31351e0902ca7b88bb780d8e27fec
Saurabh Yadav <saurabhklu.ai@gmail.com>
2025-08-19 21:35:10 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://184.72.122.226:9090/springapp1';
+const BASE_URL = 'http://34.197.146.143:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
